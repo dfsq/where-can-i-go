c1b1acccfb19b9bcb2d9deb679f12f5ee5a21410
dfsq <dfsq.dfsq@gmail.com>
2014-02-25 00:14:46 +0300

    Fixing tests.

--- test/spec/controllers/infoScreenController.js
+++ test/spec/controllers/infoScreenController.js
@@ -4,16 +4,24 @@ describe('Controller: infoScreenController', function () {
 
 	var infoController,
 		$rootScope,
-		$scope;
+		$scope,
+		$httpBackend;
 
 	// load the controller's module
 	beforeEach(module('whereCanIGo'));
 
 	// Initialize the controller and a mock scope
-	beforeEach(inject(function($controller, _$rootScope_) {
+	beforeEach(inject(function($controller, _$rootScope_, $injector) {
+
+		$httpBackend = $injector.get('$httpBackend');
 
 		$rootScope = _$rootScope_;
 		$scope = $rootScope.$new();
+
+		// Set up mock http
+		$httpBackend.when('GET', 'https://api.mongolab.com/api/1/databases/wherecanigo/collections/countries?apiKey=IXGMQbrk_gGDz-BsX-CCcYAsfEEcsuI3&fo=true&q=%7B%7D').respond(
+			{"code": "BY" , "name": "Belarus"}
+		);
 
 		infoController = $controller('infoScreenController', {
 			$scope: $scope
@@ -24,27 +32,13 @@ describe('Controller: infoScreenController', function () {
 	}));
 
 
-	it('should set property "loading" to "false"', function () {
-		expect($rootScope.loading).toBeFalsy();
-	});
-
-
-	it('should set loading=true on $rootScope.country change', function() {
-
-		// Emulate country selection by user
-		$rootScope.country = 'BY';
-		$rootScope.$digest();
-
+	it('should set property "loading" to "true"', function () {
 		expect($scope.loading).toBeTruthy();
 	});
 
-	it('should set scope.info={} on $rootScope.country change', function() {
-
-		// Emulate country selection by user
-		$rootScope.country = 'BY';
-		$rootScope.$digest();
-
-		expect($scope.info).toEqual({});
+	it('should load data for country code BY', function() {
+		$httpBackend.flush();
+		expect($rootScope.country.code).toBe('BY');
 	});
 
 });
