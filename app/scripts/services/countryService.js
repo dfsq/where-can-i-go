1ebd046d7148f2f10514b5840c99436dd1c0348e
dfsq <dfsq.dfsq@gmail.com>
2014-03-08 21:44:25 +0300

    Country service caching requests.

--- app/scripts/services/countryService.js
+++ app/scripts/services/countryService.js
@@ -1,13 +1,42 @@
 'use strict';
 
-app.factory('countryService', ['$http', function($http) {
+app.factory('countryService', ['$http', '$q', function($http, $q) {
+
+	// Store previously loaded data in cache
+	var requestCache = {};
+
 	return {
+		/**
+		 * Check/get data from the cache.
+		 * @returns {*|null}
+		 */
+		getCache: function(key) {
+			return requestCache[key] || null;
+		},
+
+		/**
+		 * Set the cache of the request.
+		 */
+		setCache: function(key, data) {
+			requestCache[key] = data;
+		},
+
 		/**
 		 * Get countries for passed one.
 		 * @param {Object} query
-		 * @return {angular.promise}
+		 * @return {promise}
 		 */
 		from: function(query) {
+
+			var self  = this,
+				cacheData = this.getCache(query.code);
+
+			if (cacheData) {
+				var deferred = $q.defer();
+				deferred.resolve(cacheData);
+				console.log('from cache', cacheData);
+				return deferred.promise;
+			}
 
 			return $http.get('/api/from/' + query.code)
 
@@ -15,6 +44,7 @@ app.factory('countryService', ['$http', function($http) {
 				var data;
 				if (response.data && typeof response.data === 'object') {
 					data = response.data;
+					self.setCache(query.code, response.data);
 				}
 				else {
 					data = query;
